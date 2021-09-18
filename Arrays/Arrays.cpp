4224216c89700af4fe657ae496adabb7d96f3168
Hale_fp67@itstepru.onmicrosoft.com <Hale_fp67@itstepru.onmicrosoft.com>
2021-09-18 20:17:41 +0300

    Add project files.

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,21 @@
+﻿// Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//std::cout << "Hello World!\n";
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+    const int n = 5;
+    cout << "Введите значения элементов 5 штук:";
+
+    int arr[n] = { 3,5,8,13,21 };
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+}
+
+
