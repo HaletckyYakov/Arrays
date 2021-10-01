32d5a292461f491f3a1dd98f5a5717e697adda25
Hale_fp67@itstepru.onmicrosoft.com <Hale_fp67@itstepru.onmicrosoft.com>
2021-10-01 14:32:04 +0300

    1 домашнее задание

--- Arrays/Arrays.cpp
+++ Arrays/Arrays.cpp
@@ -8,12 +8,15 @@ void main()
 {
     setlocale(LC_ALL, "rus");
     const int n = 5;
-    cout << "Введите значения элементов 5 штук:";
+    
+
+    
 
     int arr[n] = { 3,5,8,13,21 };
-    for (int i = 0; i < n; i++)
+    for (int i = n-1; i >=0; i--)
     {
-        cout << arr[n-i] << "\t";
+        
+        cout << arr[i] << "\t";
     }
     cout << endl;
 }
